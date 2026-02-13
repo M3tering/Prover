a2ea41276f4ddf0535e75a0e751d9d39941d226e
Godwin <godwinaquinas@gmail.com>
2026-02-13 20:54:21 +0100

    update program build args

--- node/build.rs
+++ node/build.rs
@@ -1,5 +1,10 @@
-use sp1_build::build_program_with_args;
+use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    build_program_with_args("../program", Default::default())
+    let args = BuildArgs {
+        docker: true,
+        elf_name: Some("energy-tracker-program".to_string()),
+        ..Default::default()
+    };
+    build_program_with_args("../program", args)
 }
