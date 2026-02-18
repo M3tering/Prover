e8b88ccb5efe1f80f0f4aafcc26d586f8a8f62be
Godwin <godwinaquinas@gmail.com>
2026-02-18 14:57:25 +0100

    add batch payload endpoint

--- node/build.rs
+++ node/build.rs
@@ -1,10 +1,10 @@
-use sp1_build::{build_program_with_args, BuildArgs};
+use sp1_build::build_program_with_args;
 
 fn main() {
-    let args = BuildArgs {
-        docker: true,
-        elf_name: Some("energy-tracker-program".to_string()),
-        ..Default::default()
-    };
-    build_program_with_args("../program", args)
+    // let args = BuildArgs {
+    //     docker: true,
+    //     elf_name: Some("energy-tracker-program".to_string()),
+    //     ..Default::default()
+    // };
+    build_program_with_args("../program", Default::default())
 }
