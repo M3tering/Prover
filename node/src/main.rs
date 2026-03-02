458c12f8a13cd732f69320450fff74cd56dad340
Godwin <godwinaquinas@gmail.com>
2026-03-02 14:00:26 +0100

    update batch payload option

--- node/src/main.rs
+++ node/src/main.rs
@@ -155,7 +155,7 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/batch-payload", post(batch_payload_handler))
+        .route("/batch-payloads", post(batch_payload_handler))
         .route("/health", get(health))
         .route("/run_prover", get(run_prover_handler))
         .route("/vkey", get(get_prover_vkey))
